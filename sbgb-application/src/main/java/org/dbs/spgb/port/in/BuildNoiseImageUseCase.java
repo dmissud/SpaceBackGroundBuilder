232993999546fd34d6a00a99beb709bbc847966c
Daniel Missud <daniel@missud.eu>
2026-02-01 18:19:57 +0100

    refactor(usecase): remove @FunctionalInterface annotation from noise image use cases
    Took 5 minutes

--- sbgb-application/src/main/java/org/dbs/spgb/port/in/BuildNoiseImageUseCase.java
+++ sbgb-application/src/main/java/org/dbs/spgb/port/in/BuildNoiseImageUseCase.java
@@ -2,7 +2,6 @@ package org.dbs.spgb.port.in;
 
 import java.io.IOException;
 
-@FunctionalInterface
 public interface BuildNoiseImageUseCase {
     byte[] buildNoiseImage(ImageRequestCmd imageRequestCmd) throws IOException;
 }
--- sbgb-application/src/main/java/org/dbs/spgb/port/in/CreateNoiseImageUseCase.java
+++ sbgb-application/src/main/java/org/dbs/spgb/port/in/CreateNoiseImageUseCase.java
@@ -4,7 +4,6 @@ import org.dbs.spgb.domain.model.NoiseImage;
 
 import java.io.IOException;
 
-@FunctionalInterface
 public interface CreateNoiseImageUseCase {
     //
     NoiseImage createNoiseImage(ImageRequestCmd imageRequestCmd) throws IOException;
