59ec69a499896df9508a3e086d4eb701d82725b3
Daniel Missud <daniel@missud.eu>
2026-02-01 18:20:27 +0100

    feat(dto): extend NoiseImageDTO with additional fields
    - Added `id`, `description`, `note`, `imageStructure`, and `imageColor` fields.
    - Included Lombok @Setter annotation for mutability.
    - Initialized `_links` field with a default `HashMap`.
    
    Took 30 seconds

--- sbgb-exposition/src/main/java/org/dbs/spgb/spgbexposition/resources/dto/NoiseImageDTO.java
+++ sbgb-exposition/src/main/java/org/dbs/spgb/spgbexposition/resources/dto/NoiseImageDTO.java
@@ -1,11 +1,22 @@
 package org.dbs.spgb.spgbexposition.resources.dto;
 
 import lombok.Getter;
+import lombok.Setter;
+import org.dbs.spgb.domain.model.ImageColor;
+import org.dbs.spgb.domain.model.ImageStructure;
 import org.springframework.hateoas.Link;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Getter
+@Setter
 public class NoiseImageDTO {
-    private Map<String, Link> _links;
+    private UUID id;
+    private String description;
+    private int note;
+    private ImageStructure imageStructure;
+    private ImageColor imageColor;
+    private Map<String, Link> _links = new HashMap<>();
 }
