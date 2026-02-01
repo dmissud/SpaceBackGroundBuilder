fa141a3dace373664741d8d935c9dc6ac61b604a
Daniel Missud <daniel@missud.eu>
2026-02-01 18:20:41 +0100

    feat(service): implement noise image creation with repository integration
    - Added `NoiseImageRepository` dependency to `ImagesService`.
    - Implemented `createNoiseImage` method to construct and persist `NoiseImage` entities.
    - Used `@RequiredArgsConstructor` for dependency injection in `ImagesService`.
    
    Took 6 seconds

--- sbgb-application/src/main/java/org/dbs/spgb/domain/service/ImagesService.java
+++ sbgb-application/src/main/java/org/dbs/spgb/domain/service/ImagesService.java
@@ -1,21 +1,25 @@
 package org.dbs.spgb.domain.service;
 
+import lombok.RequiredArgsConstructor;
 import org.dbs.spgb.common.UseCase;
-import org.dbs.spgb.domain.model.DefaultNoiseColorCalculator;
-import org.dbs.spgb.domain.model.NoiseImage;
-import org.dbs.spgb.domain.model.NoiseImageCalculator;
+import org.dbs.spgb.domain.model.*;
 import org.dbs.spgb.port.in.BuildNoiseImageUseCase;
 import org.dbs.spgb.port.in.CreateNoiseImageUseCase;
 import org.dbs.spgb.port.in.ImageRequestCmd;
+import org.dbs.spgb.port.out.NoiseImageRepository;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.UUID;
 
 @UseCase
+@RequiredArgsConstructor
 public class ImagesService implements BuildNoiseImageUseCase, CreateNoiseImageUseCase {
+
+    private final NoiseImageRepository noiseImageRepository;
 
     @Override
     public byte[] buildNoiseImage(ImageRequestCmd imageRequestCmd) throws IOException {
@@ -47,6 +51,29 @@ public class ImagesService implements BuildNoiseImageUseCase, CreateNoiseImageUs
 
     @Override
     public NoiseImage createNoiseImage(ImageRequestCmd imageRequestCmd) throws IOException {
-        return null;
+        byte[] imageBytes = buildNoiseImage(imageRequestCmd);
+
+        ImageStructure structure = new ImageStructure(
+                imageRequestCmd.getSizeCmd().getHeight(),
+                imageRequestCmd.getSizeCmd().getWidth(),
+                imageRequestCmd.getSizeCmd().getSeed());
+
+        ImageColor color = new ImageColor(
+                imageRequestCmd.getColorCmd().getBack(),
+                imageRequestCmd.getColorCmd().getMiddle(),
+                imageRequestCmd.getColorCmd().getFore(),
+                imageRequestCmd.getColorCmd().getBackThreshold(),
+                imageRequestCmd.getColorCmd().getMiddleThreshold());
+
+        NoiseImage noiseImage = new NoiseImage(
+                UUID.randomUUID(),
+                "Noise Image",
+                0,
+                structure,
+                color,
+                imageBytes
+        );
+
+        return noiseImageRepository.save(noiseImage);
     }
 }
