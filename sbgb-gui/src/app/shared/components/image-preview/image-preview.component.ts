73b4f87faabcf14137957db4f6e6f64cfe5a9abe
Daniel Missud <daniel@missud.eu>
2026-02-27 11:24:56 +0100

    feat(sbgb-gui): add real-size toggle button on image preview

--- sbgb-gui/src/app/shared/components/image-preview/image-preview.component.ts
+++ sbgb-gui/src/app/shared/components/image-preview/image-preview.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatTooltip} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-image-preview',
   standalone: true,
-  imports: [MatProgressSpinner],
+  imports: [MatProgressSpinner, MatIconButton, MatIcon, MatTooltip],
   templateUrl: './image-preview.component.html',
   styleUrl: './image-preview.component.scss'
 })
@@ -14,4 +17,10 @@ export class ImagePreviewComponent {
   @Input() emptyMessage: string = 'Aucune image générée';
   @Input() emptyHint: string = 'Cliquez sur "Générer aperçu" pour créer votre image';
   @Input() altText: string = 'Generated image';
+
+  realSize = false;
+
+  toggleRealSize(): void {
+    this.realSize = !this.realSize;
+  }
 }
